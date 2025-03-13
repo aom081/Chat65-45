3a25cc117044f5beb09c1d6308542232c5ce366b
aom081 <matemsom@gmail.com>
2025-03-13 13:37:57 +0700

    Initialize frontend with Vite and React; add basic pages and styles

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { Route, Routes, Navigate } from "react-router";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Setting from "./pages/Setting";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+
+function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const theme = "dark";
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUp /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route path="/setting" element={<Setting />} />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,67 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import api from "../services/api.js";
+
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  checkAuth: async () => {
+    try {
+      const res = await api.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in CheckAuth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await api.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Sign Up Failed");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await api.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success(" Logged in Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Log in Failed");
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await api.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Log out Failed");
+    }
+  },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await api.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Update Profile Failed");
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
+}));
